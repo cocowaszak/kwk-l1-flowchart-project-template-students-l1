b5abebb01812c3335f8dae068a28a64fa8fc04cb
cocowaszak <colettewaszak@gmail.com>
2018-06-14 16:55:29 +0000

    automatically backed up by learn

--- model.rb
+++ model.rb
@@ -16,7 +16,7 @@ def quiz(answer1, answer2)
       results += 3
     end
     
-    answers = [ "You go above and beyond when it comes to being eco-friendly. You reduce, re-use, buy organic, buy green, and recycle every chance you get and respect the earth and environment. You are willing to change your daily, personal life to make a difference in the world. You are doing your part to protect and preserve the environment for future generations. You set a great example for how all people should treat the earth and we thank you for being so eco-conscious and earth-sensitive." , "You doing decently well",  , "idk"]
+    answers = [ "You go above and beyond when it comes to being eco-friendly. You reduce, re-use, buy organic, buy green, and recycle every chance you get and respect the earth and environment. You are willing to change your daily, personal life to make a difference in the world. You are doing your part to protect and preserve the environment for future generations. You set a great example for how all people should treat the earth and we thank you for being so eco-conscious and earth-sensitive." , "You doing decently well", "You are pretty eco-friendly! You do your part to re-use, reduce and recycle and to be as energy efficient as possible. Although you take some steps toward a greener tomorrow, you are still stuck in the past when it comes to making a large scale change. But it's never too late to change your habits for a greener tomorrow!" , ]
     if results >= 10 && results < 3
         return answers[0]
         elsif results >= 14 && results < 5
