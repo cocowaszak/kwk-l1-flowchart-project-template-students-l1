219e8a3beae3c73d5e4f681ab6c5e3e9c87b6e37
cocowaszak <colettewaszak@gmail.com>
2018-06-14 17:44:49 +0000

    automatically backed up by learn

--- app_controller.rb
+++ app_controller.rb
@@ -1,6 +1,6 @@
   post '/' do 
 
-    @answer = quiz(params[:recycle], params[:lights], params[:waterbottle], params[:clothes], [:])
+    @answer = quiz(params[:recycle], params[:lights], params[:waterbottle], params[:clothes], [:shower], [:charger])
 
     return erb :results 
   end 
